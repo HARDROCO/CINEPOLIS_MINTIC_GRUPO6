6bdb9da1f104d30d6bf493e8fa227b8e2ba2689d
Laura Rhenals <laurhenals@gmail.com>
2021-10-16 19:12:11 -0500

    actualizacion metodos

--- app.py
+++ app.py
@@ -4,39 +4,53 @@ from flask import send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
+#Listas de prueba
+lista_peliculas = ["pelicula1", "pelicula2", "pelicula3"]
+lista_usuarios = ["Juan", "Pedro", "Maria"]
+
+@app.route('/', methods=['GET'])
 def index():
 
     return render_template('peliculas/index.html')
 
-@app.route('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
 
     return render_template('peliculas/signin.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
 
     return render_template('peliculas/signup.html')
 
-@app.route('/forgetpass')
+@app.route('/forgetpass', methods=['GET', 'POST'])
 def forgetpass ():
 
     return render_template('peliculas/forgetpass.html')
 
-@app.route('/cartelera/')
+@app.route('/cartelera/', methods=['GET'])
 def cartelera():
     return render_template('peliculas/cartelera.html')
 
-@app.route('/perfil/')
-def perfil():
-    return render_template('peliculas/perfil.html')
+@app.route('/pelicula/<id_pelicula>', methods=['GET'])
+def pelicula(id_pelicula):
+    if id_pelicula in lista_peliculas:
+        return render_template('peliculas/pelicula.html')
+    else:
+        return redirect(url_for('cartelera'))
 
-@app.route('/comidas/')
+@app.route('/perfil/<id_usuario>', methods=['GET'])
+def perfil(id_usuario):
+    if id_usuario in lista_usuarios:
+        return render_template('peliculas/perfil.html')
+    else:
+        return redirect(url_for('signin'))
+
+@app.route('/comidas/', methods=['GET'])
 def comidas():
     return render_template('peliculas/comidas.html')
 
-@app.route('/proximamente/')
+@app.route('/proximamente/', methods=['GET'])
 def proximamente():
     return render_template('peliculas/proximamente.html')
 
